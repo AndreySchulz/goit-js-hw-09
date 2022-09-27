652c0657a6e691d26a1513915f71f8b434c0cb4a
AndreySchulz <51944693+AndreySchulz@users.noreply.github.com>
2022-09-27 13:06:05 +0000

    Deploying to gh-pages from @ AndreySchulz/goit-js-hw-09@865031db6ec15af2ac102bd7725f8ac29fc713eb 🚀

---
+++ 01-color-switcher.6ab1768c.js
@@ -0,0 +1,2 @@
+!function(){var t={bodyDocument:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")},e=null;t.btnStart.addEventListener("click",(function(){t.btnStart.setAttribute("disabled","disabled"),e=setInterval((function(){t.bodyDocument.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.removeAttribute("disabled","disabled"),clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.6ab1768c.js.map
